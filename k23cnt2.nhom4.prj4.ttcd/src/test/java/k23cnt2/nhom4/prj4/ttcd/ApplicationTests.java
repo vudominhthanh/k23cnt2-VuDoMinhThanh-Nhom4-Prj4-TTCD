0fcd75401b2ff97445b026a7d0ec614d7a6fa8cc
Thành Vũ <tawoz131105@gmail.com>
2026-04-11 23:51:57 +0700

    file root

---
+++ k23cnt2.nhom4.prj4.ttcd/src/test/java/k23cnt2/nhom4/prj4/ttcd/ApplicationTests.java
@@ -0,0 +1,13 @@
+package k23cnt2.nhom4.prj4.ttcd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
